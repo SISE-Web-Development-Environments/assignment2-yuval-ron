a7a4406d97dd39a8298829cd3d22e8b215303b0d
yuvalkh <45626643+yuvalkh@users.noreply.github.com>
2020-04-28 18:08:08 +0300

    added settings before game

--- app.js
+++ app.js
@@ -12,6 +12,19 @@ var timeInterval;
 var lastMoved = "right";
 var ghosts = [];
 var lives = 5;
+var amountOfTotalFood = 50;
+var foodEaten;
+var moveRight = 39;
+var moveLeft = 37;
+var moveUp = 38;
+var moveDown = 40;
+var setWay;
+var firstFoodColor;
+var firstFoodPoints;
+var secondFoodColor;
+var secondFoodPoints;
+var thirdFoodColor;
+var thirdFoodPoints;
 
 $(document).ready(function () {
 	context = canvas.getContext("2d");
@@ -30,10 +43,64 @@ $(document).ready(function () {
 		},
 		false
 	);
-	ghostsInterval = setInterval(moveGhostRandomly, 200);
-	interval = setInterval(UpdatePosition, 100);
-	timeInterval = setInterval(countTimer, 100);
 });
+
+function moveToGame() {//function that is being called after finishing to edit the settings
+	firstFoodColor = document.getElementById("firstFoodColor").value;
+	firstFoodPoints = parseInt(document.getElementById("firstFoodPoints").value);
+	secondFoodColor = document.getElementById("secondFoodColor").value;
+	secondFoodPoints = parseInt(document.getElementById("secondFoodPoints").value);
+	thirdFoodColor = document.getElementById("thirdFoodColor").value;
+	thirdFoodPoints = parseInt(document.getElementById("thirdFoodPoints").value);
+	amountOfTotalFood = document.getElementById("amountOfFood").value;
+	document.getElementById("gameboard").style.display = "block";
+	Start();
+}
+
+function setMoveRight() {
+	document.removeEventListener('keydown', keydownHandler, false);
+	setWay = "right";
+	document.addEventListener('keydown', keydownHandler, false);
+	window.alert("Press a key you want");
+}
+
+function setMoveLeft() {
+	document.removeEventListener('keydown', keydownHandler, false);
+	setWay = "left";
+	document.addEventListener('keydown', keydownHandler, false);
+	window.alert("Press a key you want");
+}
+
+function setMoveUp() {
+	document.removeEventListener('keydown', keydownHandler, false);
+	setWay = "up";
+	document.addEventListener('keydown', keydownHandler, false);
+	window.alert("after exiting this message, press any key you want");
+}
+
+function setMoveDown() {
+	document.removeEventListener('keydown', keydownHandler, false);
+	setWay = "down";
+	document.addEventListener('keydown', keydownHandler, false);
+	window.alert("Press a key you want");
+}
+
+function keydownHandler(e) {
+	if (setWay == "right") {
+		document.getElementById("RightMove").value = e.code;
+		moveRight = e.keyCode;
+	} else if (setWay == "left") {
+		document.getElementById("LeftMove").value = e.code;
+		moveLeft = e.keyCode;
+	} else if (setWay == "up") {
+		document.getElementById("UpMove").value = e.code;
+		moveUp = e.keyCode;
+	} else if (setWay == "down") {
+		document.getElementById("DownMove").value = e.code;
+		moveDown = e.keyCode;
+	}
+	document.removeEventListener('keydown', keydownHandler, false);
+}
 
 function addGhost(ghostColor, startX, startY) {
 	let ghost = {
@@ -60,19 +127,36 @@ function countTimer() {
 		}
 	}
 	Draw();
+	if (foodEaten == amountOfTotalFood) {//the player ate all the food and finished the game
+		window.clearInterval(interval);
+		window.clearInterval(ghostsInterval);
+		window.clearInterval(timeInterval);
+		window.alert("Game completed");
+	}
 }
 
 function restartGame() {
 	window.clearInterval(interval);
 	window.clearInterval(ghostsInterval);
 	window.clearInterval(timeInterval);
-	ghostsInterval = setInterval(moveGhostRandomly, 200);
+	
+	document.getElementById("chooseSettings").style.display = "block";
+	document.getElementById("gameboard").style.display = "none";
+
+	/*ghostsInterval = setInterval(moveGhostRandomly, 200);
 	interval = setInterval(UpdatePosition, 100);
-	timeInterval = setInterval(countTimer, 100);
-	Start();
+	timeInterval = setInterval(countTimer, 100);*/
+	
+	//Start();
 }
 
 function Start() {
+	ghostsInterval = setInterval(moveGhostRandomly, 200);
+	interval = setInterval(UpdatePosition, 100);
+	timeInterval = setInterval(countTimer, 100);
+	keysDown = [];
+	foodEaten = 0;
+	let totalFoodRemain = amountOfTotalFood;
 	lives = 5;
 	document.getElementById("lives").innerHTML = "Lives: " + lives
 	remainingTime = 50.0;
@@ -84,8 +168,10 @@ function Start() {
 	board = new Array();
 	score = 0;
 	pac_color = "yellow";
-	var cnt = 100;
-	var food_remain = 50;
+	var cnt = 176;
+	var firstFoodRemain = Math.ceil(0.6 * totalFoodRemain);
+	var secondFoodRemain = Math.floor(0.3 * totalFoodRemain);
+	var thirdFoodRemain = Math.floor(0.1 * totalFoodRemain);
 	var pacman_remain = 1;
 	start_time = new Date();
 	for (let i = 0; i < 16; i++) {//every column
@@ -128,10 +214,22 @@ function Start() {
 				board[i][j] = 4;
 			} else {
 				let randomNum = Math.random();
-				if (randomNum <= (1.0 * food_remain) / cnt) {
-					food_remain--;
-					board[i][j] = 1;
-				} else if (randomNum < (1.0 * (pacman_remain + food_remain)) / cnt) {
+				if (randomNum <= (1.0 * totalFoodRemain) / cnt) {
+					randomNum = Math.floor(Math.random() * 3 + 1);
+					if (randomNum == 1 && firstFoodRemain > 0) {
+						board[i][j] = 1;
+						firstFoodRemain--;
+						totalFoodRemain--;
+					} else if (randomNum == 2 && secondFoodRemain > 0) {
+						board[i][j] = 10;
+						secondFoodRemain--;
+						totalFoodRemain--;
+					} else if (randomNum == 3 && thirdFoodRemain > 0) {
+						board[i][j] = 11;
+						thirdFoodRemain--;
+						totalFoodRemain--;
+					}
+				} else if (randomNum < (1.0 * (pacman_remain + totalFoodRemain)) / cnt) {
 					shape.x = i;
 					shape.y = j;
 					pacman_remain--;
@@ -143,38 +241,51 @@ function Start() {
 			}
 		}
 	}
-	while (food_remain > 0) {
-		var emptyCell = findRandomEmptyCell(board);
-		board[emptyCell[0]][emptyCell[1]] = 1;
-		food_remain--;
+	while (totalFoodRemain > 0) {
+		let emptyCell;
+		for (let i = 0; i < thirdFoodRemain; i++) {
+			emptyCell = findRandomEmptyCell(board);
+			board[emptyCell[0]][emptyCell[1]] = 11;
+			totalFoodRemain--;
+		}
+		for (let i = 0; i < secondFoodRemain; i++) {
+			emptyCell = findRandomEmptyCell(board);
+			board[emptyCell[0]][emptyCell[1]] = 10;
+			totalFoodRemain--;
+		}
+		for (let i = 0; i < firstFoodRemain; i++) {
+			emptyCell = findRandomEmptyCell(board);
+			board[emptyCell[0]][emptyCell[1]] = 1;
+			totalFoodRemain--;
+		}
 	}
 
 }
 
 function findRandomEmptyCell(board) {
-	var i = Math.floor(Math.random() * 16 + 1);
-	var j = Math.floor(Math.random() * 11 + 1);
+	let i = Math.floor(Math.random() * 14 + 1);
+	let j = Math.floor(Math.random() * 10 + 1);
 	while (board[i][j] != 0) {
-		i = Math.floor(Math.random() * 16 + 1);
-		j = Math.floor(Math.random() * 11 + 1);
+		i = Math.floor(Math.random() * 14 + 1);
+		j = Math.floor(Math.random() * 10 + 1);
 	}
 	return [i, j];
 }
 
 function GetKeyPressed() {
-	if (keysDown[38]) {
+	if (keysDown[moveUp]) {
 		lastMoved = "up";
 		return 1;
 	}
-	if (keysDown[40]) {
+	if (keysDown[moveDown]) {
 		lastMoved = "down";
 		return 2;
 	}
-	if (keysDown[37]) {
+	if (keysDown[moveLeft]) {
 		lastMoved = "left";
 		return 3;
 	}
-	if (keysDown[39]) {
+	if (keysDown[moveRight]) {
 		lastMoved = "right";
 		return 4;
 	}
@@ -189,7 +300,7 @@ function loseLiveAndRestart() {
 		ghosts[0].y = 10;
 
 		ghosts[1].x = 15;
-		ghosts[1].x = 10;
+		ghosts[1].y = 10;
 
 		ghosts[2].x = 0;
 		ghosts[2].y = 0;
@@ -277,12 +388,24 @@ function Draw() {
 					context.fillStyle = "black"; //color
 					context.fill();
 				}
-			} else if (board[i][j] == 1) {// food
+			} else if (board[i][j] == 1) {// food1
 				context.beginPath();
 				context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
-				context.fillStyle = "gold"; //color
+				context.fillStyle = firstFoodColor; //color
 				context.fill();
-			} else if (board[i][j] == 4) {// wall
+			} else if (board[i][j] == 10) {// food2
+				context.beginPath();
+				context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
+				context.fillStyle = secondFoodColor; //color
+				context.fill();
+			}
+			else if (board[i][j] == 11) {// food3
+				context.beginPath();
+				context.arc(center.x, center.y, 5, 0, 2 * Math.PI); // circle
+				context.fillStyle = thirdFoodColor; //color
+				context.fill();
+			}
+			else if (board[i][j] == 4) {// wall
 				context.beginPath();
 				context.rect(center.x - 20, center.y - 20, 40, 40);
 				context.fillStyle = "grey"; //color
@@ -290,7 +413,7 @@ function Draw() {
 			}
 		}
 	}
-	for (let i = 0; i < ghosts.length; i++) {
+	for (let i = 0; i < ghosts.length; i++) {//draw ghosts
 		let center = new Object();
 		center.x = ghosts[i].x * 40 + 20;
 		center.y = ghosts[i].y * 40 + 20;
@@ -319,12 +442,13 @@ function Draw() {
 }
 
 function moveGhostRandomly() {
-
 	for (let i = 0; i < ghosts.length; i++) {
 		let randomNumber = Math.floor(Math.random() * 4) + 1;
 		moveGhost(ghosts[i], randomNumber);
 	}
 }
+
+
 
 function UpdatePosition() {
 	board[shape.x][shape.y] = 0;
@@ -356,10 +480,18 @@ function UpdatePosition() {
 		}
 	}
 	if (board[shape.x][shape.y] == 1) {//if it's a normal score we raise the score
-		score++;
+		score += firstFoodPoints;
+		foodEaten++;
+	}
+	if (board[shape.x][shape.y] == 10) {//if it's a middle score we raise the score
+		score += secondFoodPoints;
+		foodEaten++;
+	}
+	if (board[shape.x][shape.y] == 11) {//if it's a high score we raise the score
+		score += thirdFoodPoints;
+		foodEaten++;
 	}
 	board[shape.x][shape.y] = 2;
-	//Draw();
 	for (let i = 0; i < ghosts.length; i++) {
 		if (shape.x == ghosts[i].x && shape.y == ghosts[i].y) {
 			loseLiveAndRestart();
@@ -370,42 +502,65 @@ function UpdatePosition() {
 	if (score >= 20 && time_elapsed <= 10) {
 		pac_color = "green";
 	}
-	if (score == 50) {
-		window.clearInterval(interval);
-		window.alert("Game completed");
-	}
 }
-
-
 
 function moveGhost(creature, way) {
 	if (way == 1) {//up
 		if (creature.y > 0 && board[creature.x][creature.y - 1] != 4 && !((creature.y == 5 || creature.y == 6) && (creature.x < 3 || creature.x > 12))) {
+			for (let i = 0; i < ghosts.length; i++) {
+				if (creature.x == ghosts[i].x && creature.y - 1 == ghosts[i].y) {//if there is already a ghost in the spot so we stay in place
+					return;
+				}
+			}
 			creature.y--;
 		}
 	}
 	if (way == 2) {//down
 		if (creature.y < 10 && board[creature.x][creature.y + 1] != 4 && !((creature.y == 5 || creature.y == 4) && (creature.x < 3 || creature.x > 12))) {
+			for (let i = 0; i < ghosts.length; i++) {
+				if (creature.x == ghosts[i].x && creature.y + 1 == ghosts[i].y) {//if there is already a ghost in the spot so we stay in place
+					return;
+				}
+			}
 			creature.y++;
 		}
 	}
 	if (way == 3) {//left
 		if (creature.x > 0 && board[creature.x - 1][creature.y] != 4) {
+			for (let i = 0; i < ghosts.length; i++) {
+				if (creature.x - 1 == ghosts[i].x && creature.y == ghosts[i].y) {//if there is already a ghost in the spot so we stay in place
+					return;
+				}
+			}
 			creature.x--;
 		}
 		else if (creature.y == 5 && creature.x == 0) {//need to teleport him to the other side
+			for (let i = 0; i < ghosts.length; i++) {
+				if (ghosts[i].x == 15 && creature.y == ghosts[i].y) {//if there is already a ghost in the spot so we stay in place
+					return;
+				}
+			}
 			creature.x = 15;
 		}
 	}
 	if (way == 4) {//right
 		if (creature.x < 15 && board[creature.x + 1][creature.y] != 4) {
+			for (let i = 0; i < ghosts.length; i++) {
+				if (creature.x + 1 == ghosts[i].x && creature.y == ghosts[i].y) {//if there is already a ghost in the spot so we stay in place
+					return;
+				}
+			}
 			creature.x++;
 		}
 		else if (creature.y == 5 && creature.x == 15) {//need to teleport him to the other side
+			for (let i = 0; i < ghosts.length; i++) {
+				if (ghosts[i].x == 0 && creature.y == ghosts[i].y) {//if there is already a ghost in the spot so we stay in place
+					return;
+				}
+			}
 			creature.x = 0;
 		}
 	}
-	//Draw();
 	if (shape.x == creature.x && shape.y == creature.y) {
 		loseLiveAndRestart();
 	}
